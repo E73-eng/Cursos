6e415e91fd0d3658ee882dfe1d2e1682a9dadeda
edtop <edtops59@gmail.com>
2025-10-22 20:19:52 -0600

    Parte 2 - Uso de props para pasar datos

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Total from './components/Total'
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
-  const exercises1 = 10
+  const exercises1 = 120
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
