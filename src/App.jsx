8d785174ae4a49636b46f1a64a407ed11f688e66
edtop <edtops59@gmail.com>
2025-11-02 12:03:45 -0600

    Ejercicio 7 --- Pasando-estado-a-hijos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Header from './components/Header'
 import Content from './components/Content'
 import Total from './components/Total'
 import { useState } from 'react'
+import Display from './components/Display'
+import Button from './components/Button'
 
 
 const App = () => {  
@@ -9,16 +11,20 @@ const App = () => {
   const [ counter, setCounter ] = useState(0)  
 
   const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const resetToZero = () => setCounter(0)  
 
   return (
+    
     <div>
-      <div>{counter}</div>
 
-      <button onClick={increaseByOne}>plus</button>
-      <button onClick={resetToZero}>reset</button>
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text='plus'/>
+      <Button onClick={decreaseByOne} text='minus'/>
+      <Button onClick={resetToZero} text='reset'/>      
 
-    </div>
+    </div>      
+    
   )
 }
 
