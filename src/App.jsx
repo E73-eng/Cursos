daf197dc869e42241d7768df5e08a5f349ae057c
edtop <edtops59@gmail.com>
2025-11-02 11:25:56 -0600

    Ejercicio 5 --- Event-control

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
 import Header from './components/Header'
 import Content from './components/Content'
 import Total from './components/Total'
+import { useState } from 'react'
+
 
 const App = () => {  
 
-  const course = {
-    name: 'Half Stack application development',
-    
-    parts: [
-      { name: 'Fundamentals of React', exercises: 10 },
-      { name: 'Using props to pass data', exercises: 7 },
-      { name: 'State of a component', exercises: 14 },
-    ],
-  }
+  const [ counter, setCounter ] = useState(0)  
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>plus</button>
     </div>
   )
 }
